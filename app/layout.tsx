76e736a5c562d4c1ff41ec743f87df29936921b2
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-04-20 11:58:29 +0200

    intitalization

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="de">
       <body className={inter.className}>{children}</body>
     </html>
   );
