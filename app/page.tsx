6576bd7d1d029fcc3874bd7213aeb55515279c43
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-04-22 14:40:07 +0200

    test: tobitest

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>BITZZZ</main>
+    <div>
+      <main>BITZ</main>
+      <h2>beste App der Welt</h2>
+    </div>
   );
 }
