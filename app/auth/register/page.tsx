6c0259cbe16f641a23637623ba0ec04da2af58c6
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-04-28 12:17:42 +0200

    feat: resend working, still jwt authentication...

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,8 +1,19 @@
-
+import { signIn } from "@/auth"
 
 const Register = () => {
   return (
-    <h1>Register</h1>
+    <>
+      <h1>Register</h1>
+      <form
+        action={async (formData) => {
+          "use server"
+          await signIn("resend", formData)
+        }}
+      >
+        <input type="text" name="email" placeholder="Email" />
+        <button type="submit">Signin with Resend</button>
+      </form>
+    </>
   )
 }
 
--- auth.ts
+++ auth.ts
@@ -1,13 +1,37 @@
-import NextAuth from "next-auth"
-import google from "next-auth/providers/google"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "@/schemas/migrate"
-import authConfig from "./auth.config"
-import Resend from "next-auth/providers/resend"
-import credentials from "next-auth/providers/credentials"
+// import NextAuth from "next-auth"
+// import google from "next-auth/providers/google"
+// import { DrizzleAdapter } from "@auth/drizzle-adapter"
+// import { db } from "@/schemas/migrate"
+// import authConfig from "./auth.config"
+// import Resend from "next-auth/providers/resend"
+// import credentials from "next-auth/providers/credentials"
+// import resend from "next-auth/providers/resend"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: DrizzleAdapter(db),
-  session: { strategy: "jwt" },
-  ...authConfig
-})
+// export const { handlers, signIn, signOut, auth } = NextAuth(authConfig)
+
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { users, accounts, sessions, verificationTokens } from "./schemas/schema"
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./schemas/migrate";
+
+
+export type { Session } from "next-auth";
+
+const {
+  handlers,
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  ...authConfig,
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  session: { strategy: "jwt" }
+});
+
+export { handlers, auth, signIn, signOut };
