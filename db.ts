5fb8930e49adcd5584e387c34ee51b61d04f1a4b
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-04-30 15:58:15 +0200

    fix: revert last commit changes

--- db.ts
+++ db.ts
@@ -2,7 +2,7 @@ import { Pool } from "@neondatabase/serverless"
 import { config } from "dotenv"
 import { drizzle } from "drizzle-orm/neon-serverless"
 
-config({ path: ".env.local" })
+console.log("testo")
 
 const pool = new Pool({ connectionString: process.env.POSTGRES_URL! })
 export const db = drizzle(pool)
