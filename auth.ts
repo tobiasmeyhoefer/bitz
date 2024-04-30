b2b68a050b34d227553a17535b090b94d65849ca
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-04-30 12:42:09 +0200

    added session strategy jwt

--- auth.ts
+++ auth.ts
@@ -7,6 +7,7 @@ import authConfig from "./auth.config"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
+  session: { strategy: "jwt" },
   ...authConfig
   // providers: [
   //   google,
