f0efece2e8f565127f945097bd1301f21cc3fba0
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-04-30 09:58:15 +0200

    fix: changed worng pathes for deployment

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,19 +1,19 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import { users, UserType } from "@/schemas/schema";
 import { and, eq } from "drizzle-orm";
-import { db } from "@/schemas/migrate";
+import { db } from "@/db";
+import { users } from "@/schema";
 
-export async function getUserFromDb(email: string, password: string) {
+// export async function getUserFromDb(email: string, password: string) {
 
-  const user: UserType[] = (await db.select().from(users).where(eq(users.email, email)))
+//   const user: UserType[] = (await db.select().from(users).where(eq(users.email, email)))
 
-  if(user[0].password === password) {
-    return user[0]
-  }
+//   if(user[0].password === password) {
+//     return user[0]
+//   }
 
-  return null;
-}
+//   return null;
+// }
 
 // export async function createUser(formData) {
 //   await db.insert(users).values({
--- actions/user.ts
+++ actions/user.ts
@@ -1,13 +1,13 @@
-import { db } from "@/schemas/migrate"
-import { users, UserType } from "@/schemas/schema";
+import { db } from "@/db";
+import { users } from "@/schema";
 import { eq } from "drizzle-orm";
 
-export const getUserByEmail = async (email: string) => {
-  try {
-    const user: UserType[] = await db.select().from(users).where(eq(users.email, email))
-    return user
-  } catch {
-    return null;
-  } 
-}
+// export const getUserByEmail = async (email: string) => {
+//   try {
+//     const user: UserType[] = await db.select().from(users).where(eq(users.email, email))
+//     return user
+//   } catch {
+//     return null;
+//   } 
+// }
 
