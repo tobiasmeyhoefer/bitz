a57a0939a207501969ef0c4e2e518a7ccac3c480
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-04-30 10:09:55 +0200

    fix: deleted middleware content

--- middleware.ts
+++ middleware.ts
@@ -3,28 +3,28 @@ import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, customPages, publicR
 import { auth } from './auth'
 
 export default auth((req) => {
-  const nextUrl = req.nextUrl
-  const isLoggedIn = !!req.auth
+  // const nextUrl = req.nextUrl
+  // const isLoggedIn = !!req.auth
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
-  const isCustomRoute = customPages.includes(nextUrl.pathname)
+  // const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  // const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  // const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+  // const isCustomRoute = customPages.includes(nextUrl.pathname)
 
-  if (isApiAuthRoute || isCustomRoute) {
-    return
-  }
+  // if (isApiAuthRoute || isCustomRoute) {
+  //   return
+  // }
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
-    }
-    return
-  }
+  // if (isAuthRoute) {
+  //   if (isLoggedIn) {
+  //     return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+  //   }
+  //   return
+  // }
 
-  if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL('/', nextUrl))
-  }
+  // if (!isLoggedIn && !isPublicRoute) {
+  //   return Response.redirect(new URL('/', nextUrl))
+  // }
 })
 
 export const config = {
