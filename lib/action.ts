fb63de3139937ae2d45009e3e6fe731ffeaedc96
Niko Budic <s87786@bht-berlin.de>
2024-05-05 17:57:27 +0200

    feature: deleteProduct implemented, started working on updateProduct, Niko

--- lib/action.ts
+++ lib/action.ts
@@ -1,6 +1,8 @@
 "use server"
+import { products } from "@/schema";
 import { db } from "../db"
 import { revalidatePath } from "next/cache";
+import { eq } from "drizzle-orm";
 
 // export async function addProduct(formData:any) {
 //   const {id, title, description, price, currency, quantity, location, status} = Object.fromEntries(formData);
@@ -14,3 +16,15 @@ import { revalidatePath } from "next/cache";
 //                                       status: status,
 //                                   });
 // }
+
+// Delete function requiring productId as string
+export async function deleteProduct(productId: string) {
+    await db.delete(products).where(eq(products.id, productId));
+}
+
+// Update function requiring productData as 
+export async function updateProduct() {
+    
+}
+
+
