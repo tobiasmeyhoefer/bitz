4a9e9cb1dc03a24a26506b51b12701d90cf7816e
Dennis Blömeke <de.bloemeke@gmail.com>
2024-05-05 19:11:13 +0200

    feat: added addProduct ServerAction

--- lib/action.ts
+++ lib/action.ts
@@ -1,30 +1,35 @@
-"use server"
-import { products } from "@/schema";
-import { db } from "../db"
-import { revalidatePath } from "next/cache";
-import { eq } from "drizzle-orm";
+'use server'
+import { products } from '@/schema'
+import { db } from '../db'
+import { revalidatePath } from 'next/cache'
+import { eq } from 'drizzle-orm'
+import { auth } from '@/auth'
 
-// export async function addProduct(formData:any) {
-//   const {id, title, description, price, currency, quantity, location, status} = Object.fromEntries(formData);
-//   await db.insert(Products).values({  title: title,
-//                                       description: description,
-//                                       price: price,
-//                                       currency: currency,
-//                                       quantity: quantity,
-//                                       location: location,
-//                                       sellerId: "",  VLT mit auth an die Id kommen
-//                                       status: status,
-//                                   });
-// }
+export async function addProduct(values: any) {
+  const { title, description, price, currency, quantity, location, status } =
+    values
+  const session = await auth()
+  const id = session?.user?.id
+  if (id) {
+    await db.insert(products).values({
+      title: title,
+      description: description,
+      price: price,
+      currency: currency,
+      quantity: quantity,
+      location: location,
+      sellerId: id,
+      status: status,
+      createdAt: new Date(Date.now()),
+      image: '',
+    })
+  }
+}
 
 // Delete function requiring productId as string
 export async function deleteProduct(productId: string) {
-    await db.delete(products).where(eq(products.id, productId));
+  await db.delete(products).where(eq(products.id, productId))
 }
 
-// Update function requiring productData as 
-export async function updateProduct() {
-    
-}
-
-
+// Update function requiring productData as
+export async function updateProduct() {}
--- schema.ts
+++ schema.ts
@@ -4,73 +4,81 @@ import {
   text,
   primaryKey,
   integer,
-} from "drizzle-orm/pg-core"
-import type { AdapterAccount } from "next-auth/adapters"
- 
-export const users = pgTable("user", {
-  id: text("id")
+} from 'drizzle-orm/pg-core'
+import type { AdapterAccount } from 'next-auth/adapters'
+
+export const users = pgTable('user', {
+  id: text('id')
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
-  name: text("name"),
-  email: text("email").notNull(),
-  emailVerified: timestamp("emailVerified", { mode: "date" }),
-  image: text("image"),
+  name: text('name'),
+  email: text('email').notNull(),
+  emailVerified: timestamp('emailVerified', { mode: 'date' }),
+  image: text('image'),
 })
 
-export const products = pgTable("product", {
-  id: text("id").primaryKey().$defaultFn(() => crypto.randomUUID()),
-  title: text("title").notNull(),
-  description: text("description"),
-  price: integer("price").notNull(),
-  currency: text("currency").notNull(),
-  quantity: integer("quantity").notNull().$default(() => 1),
-  location: text("location"),
-  sellerId: text("sellerId").notNull().references(() => users.id, { onDelete: "cascade" }),
-  status: text("status").notNull().$default(() => "available"),
-  createdAt: timestamp("createdAt", { mode: "date" }).notNull(),
-  image: text(" "),
-});
+export const products = pgTable('product', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  title: text('title').notNull(),
+  description: text('description'),
+  price: integer('price').notNull(),
+  currency: text('currency').notNull(),
+  quantity: integer('quantity')
+    .notNull()
+    .$default(() => 1),
+  location: text('location'),
+  sellerId: text('sellerId')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  status: text('status')
+    .notNull()
+    .$default(() => 'available'),
+  createdAt: timestamp('createdAt', { mode: 'date' }).notNull(),
+  image: text('image'),
+})
 
 export const accounts = pgTable(
-  "account",
+  'account',
   {
-    userId: text("userId")
+    userId: text('userId')
       .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    type: text("type").$type<AdapterAccount["type"]>().notNull(),
-    provider: text("provider").notNull(),
-    providerAccountId: text("providerAccountId").notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: integer("expires_at"),
-    token_type: text("token_type"),
-    scope: text("scope"),
-    id_token: text("id_token"),
-    session_state: text("session_state"),
+      .references(() => users.id, { onDelete: 'cascade' }),
+    type: text('type').$type<AdapterAccount['type']>().notNull(),
+    provider: text('provider').notNull(),
+    providerAccountId: text('providerAccountId').notNull(),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: integer('expires_at'),
+    token_type: text('token_type'),
+    scope: text('scope'),
+    id_token: text('id_token'),
+    session_state: text('session_state'),
   },
   (account) => ({
     compoundKey: primaryKey({
       columns: [account.provider, account.providerAccountId],
     }),
-  })
+  }),
 )
- 
-export const sessions = pgTable("session", {
-  sessionToken: text("sessionToken").primaryKey(),
-  userId: text("userId")
+
+export const sessions = pgTable('session', {
+  sessionToken: text('sessionToken').primaryKey(),
+  userId: text('userId')
     .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  expires: timestamp("expires", { mode: "date" }).notNull(),
+    .references(() => users.id, { onDelete: 'cascade' }),
+  expires: timestamp('expires', { mode: 'date' }).notNull(),
 })
- 
+
 export const verificationTokens = pgTable(
-  "verificationToken",
+  'verificationToken',
   {
-    identifier: text("identifier").notNull(),
-    token: text("token").notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
+    identifier: text('identifier').notNull(),
+    token: text('token').notNull(),
+    expires: timestamp('expires', { mode: 'date' }).notNull(),
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+  }),
 )
