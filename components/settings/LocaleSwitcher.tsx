eaafec8816100fdc9d1aba5bfe103455439575a7
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-05-06 14:28:15 +0200

    fix: fixed messages

--- components/settings/LocaleSwitcher.tsx
+++ components/settings/LocaleSwitcher.tsx
@@ -1,7 +1,6 @@
 import {useLocale, useTranslations} from 'next-intl';
 import LocaleSwitcherSelect from './LocaleSwitcherSelect';
 import {locales} from '@/i18n';
-import { unstable_setRequestLocale } from 'next-intl/server';
 import { SelectItem } from '../ui/select';
 
 export default function LocaleSwitcher() {
@@ -9,7 +8,7 @@ export default function LocaleSwitcher() {
   const locale = useLocale();
 
   return (
-    <LocaleSwitcherSelect defaultValue={locale} label={t('label')}>
+    <LocaleSwitcherSelect defaultValue={t('locale', {locale: locale})} label={t('label')}>
       {locales.map((cur) => (
         <SelectItem key={cur} value={cur}>{t('locale', {locale: cur})}</SelectItem>
       ))}
