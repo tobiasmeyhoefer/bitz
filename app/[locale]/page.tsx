932d9de848cb14daa1c50f975ac76841db489911
Wolkenwolf9 <anton.kripp@gmail.com>
2024-05-07 17:55:49 +0200

    fix Link bug

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,4 @@
 import { auth } from '@/auth'
-import { Button } from '@/components/ui/button'
 import { Link, redirect } from '@/navigation'
 import { ChevronRightIcon } from '@radix-ui/react-icons'
 import dynamic from 'next/dynamic'
@@ -19,21 +18,18 @@ export default async function Home() {
       <CubeScene />
 
       {/* Call-to-Action Button  */}
-      <Button>
-        <Link href={'/auth/login'}>
-          <div className="absolute bottom-24 right-24 h-24 w-96 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-8 font-space_grotesk text-4xl text-white hover:scale-105 hover:opacity-85">
-            <div className="absolute right-9 mt-4 h-16 w-16 rounded-full bg-white">
-              <ChevronRightIcon className="relative  size-16 text-black" />
-            </div>
-            <div className="mr-12 mt-7 ">
-              <Link href={'/auth/login'}>{'Loslegen!'}</Link>
-            </div>
+
+      <Link href={'/auth/login'}>
+        <div className="absolute bottom-24 right-24 h-24 w-96 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-8 font-space_grotesk text-4xl text-white hover:scale-105 hover:opacity-85">
+          <div className="absolute right-9 mt-4 h-16 w-16 rounded-full bg-white">
+            <ChevronRightIcon className="relative size-16 text-black" />
           </div>
-        </Link>
-      </Button>
+          <div className="relative left-10 mt-7">Loslegen</div>
+        </div>
+      </Link>
 
       {/* Beschreibungstext  */}
-      <div className="absolute bottom-16 left-24 w-11 rounded-md p-5 font-space_grotesk text-black drop-shadow-2xl  md:w-7/12">
+      <div className="absolute  left-24 rounded-md p-5 font-space_grotesk text-black drop-shadow-2xl  md:bottom-16 md:w-7/12">
         <p className="text-5xl">
           <em>Bitz</em>
         </p>
