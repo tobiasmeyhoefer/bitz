e140f07ffacc228949adbfb4d710bb0ac147888b
Wolkenwolf9 <anton.kripp@gmail.com>
2024-05-07 21:24:42 +0200

    fix lang bug

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
           <div className="absolute right-6 mt-6 h-12 w-12 rounded-full bg-white md:right-9 md:mt-4 md:h-16 md:w-16">
             <ChevronRightIcon className="relative size-12 text-black md:size-16" />
           </div>
-          <div className="relative left-0 mt-7 md:left-10">t("button")</div>
+          <div className="relative left-0 mt-7 md:left-10">{t('button')}</div>
         </div>
       </Link>
 
@@ -35,9 +35,9 @@ export default async function Home() {
         <p className="text-5xl">
           <em>Bitz</em>
         </p>
-        <p className="mt-4 text-2xl">t("subtitle")</p>
-        <p className="mt-4 text-xl">t("description")</p>
-        <p className="mt-4 text-2xl">t("call")</p>
+        <p className="mt-4 text-2xl">{t('subtitle')}</p>
+        <p className="mt-4 text-xl">{t('description')}</p>
+        <p className="mt-4 text-2xl">{t('call')}</p>
       </div>
     </main>
   )
