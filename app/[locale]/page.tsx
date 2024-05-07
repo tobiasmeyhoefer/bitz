6044e1ec9e5d6a37c3aa0030c781286820b34a23
Wolkenwolf9 <anton.kripp@gmail.com>
2024-05-07 13:47:34 +0200

    feat call-to-action button

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,8 @@
 import { auth } from '@/auth'
+import { Button } from '@/components/ui/button'
+import { Link, redirect } from '@/navigation'
+import { ChevronRightIcon } from '@radix-ui/react-icons'
 import dynamic from 'next/dynamic'
-import { redirect } from '@/navigation'
 
 const CubeScene = dynamic(() => import('@/components/explosion/cubeScene'), {
   ssr: false,
@@ -15,6 +17,15 @@ export default async function Home() {
   return (
     <main className="static h-screen pt-24">
       <CubeScene />
+
+      <Button className="text-24 absolute bottom-24 right-24 h-36 w-1/4 rounded-full  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-8 font-montserrat text-4xl text-white ">
+        <div className="absolute right-9 h-24 w-24 rounded-full bg-white">
+          <ChevronRightIcon className="relative size-24 text-black" />
+        </div>
+        <div className="mr-24">
+          <Link href={'/auth/login'}>{'Loslegen!'}</Link>
+        </div>
+      </Button>
     </main>
   )
 }
