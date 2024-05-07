bbdb82653dbe49759ccc523f978b168bb8df01b6
Wolkenwolf9 <anton.kripp@gmail.com>
2024-05-07 17:22:40 +0200

    style Beschreibung

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -15,17 +15,37 @@ export default async function Home() {
   }
 
   return (
-    <main className="static h-screen pt-24">
+    <main className="static h-screen pt-16">
       <CubeScene />
 
-      <Button className="text-24 absolute bottom-24 right-24 h-36 w-1/4 rounded-full  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-8 font-montserrat text-4xl text-white ">
-        <div className="absolute right-9 h-24 w-24 rounded-full bg-white">
-          <ChevronRightIcon className="relative size-24 text-black" />
-        </div>
-        <div className="mr-24">
-          <Link href={'/auth/login'}>{'Loslegen!'}</Link>
-        </div>
-      </Button>
+      {/* Call-to-Action Button  */}
+      <Link href={'/auth/login'}>
+        <Button className="absolute bottom-24 right-24 h-36 w-1/4 rounded-full bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 px-8 font-space_grotesk text-4xl text-white ">
+          <div className="absolute right-9 h-20 w-20 rounded-full bg-white">
+            <ChevronRightIcon className="relative size-20 text-black" />
+          </div>
+          <div className="mr-24">
+            <Link href={'/auth/login'}>{'Loslegen!'}</Link>
+          </div>
+        </Button>
+      </Link>
+
+      {/* Beschreibungstext  */}
+      <div className="absolute bottom-16 left-24 w-11 rounded-md p-5 font-space_grotesk text-black drop-shadow-2xl  md:w-7/12">
+        <p className="text-5xl">
+          <em>Bitz</em>
+        </p>
+        <p className="mt-4 text-2xl">
+          von heute für morgen mit Technik versorgen
+        </p>
+        <p className="mt-4 text-xl">
+          Entdecke bei <em>Bitz</em> die neuesten Technikgeräte zu unschlagbaren
+          Preisen! Kaufe, verkaufe und tausche deine Lieblingsgadgets in einer
+          sicheren Umgebung. Trete unserer lebendigen Community bei und finde
+          noch heute dein nächstes Technik-Highlight.
+        </p>
+        <p className="mt-4 text-2xl">Jetzt anmelden und durchstarten!</p>
+      </div>
     </main>
   )
 }
