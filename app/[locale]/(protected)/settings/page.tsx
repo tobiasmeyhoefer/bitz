883e1685b4c07c659c2635c0a865802035e75826
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-05-12 00:02:28 +0200

    reverted to old strategy

--- app/[locale]/(protected)/settings/page.tsx
+++ app/[locale]/(protected)/settings/page.tsx
@@ -4,14 +4,13 @@ import { checkPasskey } from '@/lib/action'
 
 const Settings = async () => {
   const hasPasskey = await checkPasskey()
-  console.log(hasPasskey)
   return (
     <div className="flex h-full flex-col items-center px-20 py-40">
       <h1 className="mb-10 text-3xl">Settings</h1>
       <div className="flex flex-col gap-6">
         <LocaleSwitcher />
-        {/* {hasPasskey ? <p>Passkey registered</p> : <RegisterPasskey />} */}
-        <RegisterPasskey />
+        {hasPasskey ? <p>Passkey registered</p> : <RegisterPasskey />}
+        {/* <RegisterPasskey /> */}
       </div>
     </div>
   )
