8fc93291925ac5c7b29ba181997585e2705f9f09
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-05-11 22:36:47 +0200

    feat: added sign in with passkey comment in login form

--- components/auth/sign-in-passkey.tsx
+++ components/auth/sign-in-passkey.tsx
@@ -1,22 +1,27 @@
-"use client"
+'use client'
 
-import { signIn } from "next-auth/webauthn"
-import { Button } from "../ui/button"
-import { useRouter } from "@/navigation"
-import { toast } from "../ui/use-toast"
-
+import { signIn } from 'next-auth/webauthn'
+import { Button } from '../ui/button'
+import { useRouter } from '@/navigation'
 
 const SignInPasskey = () => {
-  
   const signInWithPasskey = () => {
     signIn('passkey').catch(() => {
       router.refresh()
-      return;
+      return
     })
   }
 
   const router = useRouter()
-  return <Button onClick={signInWithPasskey}>Sign in with Passkey</Button>
+  return (
+    <>
+      <Button onClick={signInWithPasskey}>Sign in with Passkey</Button>
+      <p className='mt-6'>
+        tip: create normal account, find registering
+        passkey option in the settings, enjoy
+      </p>
+    </>
+  )
 }
 
 export default SignInPasskey
