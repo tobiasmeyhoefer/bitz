cd77a23cb5915efd32e239d1116576cd125fc051
Wolkenwolf9 <anton.kripp@gmail.com>
2024-05-16 11:08:28 +0200

    feat NavBar Logo

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -28,9 +28,7 @@ export default async function Home() {
           <div className="absolute right-6 mt-6 h-12 w-12 rounded-full bg-white md:right-9 md:mt-4 md:h-16 md:w-16">
             <ChevronRightIcon className="relative size-12 text-black md:size-16" />
           </div>
-          <div className="relative left-0 mt-7 text-3xl md:left-10">
-            {t('button')}
-          </div>
+          <div className="relative left-0 mt-7 text-3xl md:left-10">{t('button')}</div>
         </div>
       </Link>
 
