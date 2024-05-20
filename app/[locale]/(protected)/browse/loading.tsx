3d01a6a5a3e030063017c7a4a0c3b2ac58806f00
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-05-20 20:28:57 +0200

    added loading and aria label

---
+++ app/[locale]/(protected)/browse/loading.tsx
@@ -0,0 +1,41 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <>
+      <div className="flex flex-wrap gap-5">
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+        <Skeleton className="h-[400px] w-[250px] rounded-lg" />
+      </div>
+    </>
+  )
+}
