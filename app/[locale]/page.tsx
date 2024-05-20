5d80aa0f3b1f7a95c69b955dfb3168fa0fa34879
Wolkenwolf9 <anton.kripp@gmail.com>
2024-05-20 20:13:28 +0200

    feat translations

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -18,12 +18,12 @@ export default async function Home() {
 
   return (
     <main className="static h-screen snap-y snap-mandatory overflow-y-scroll">
-      <section className="mt-20 flex h-screen snap-start flex-col font-space_grotesk md:flex-row">
-        <div className="flex w-full flex-col justify-center p-4 md:mr-36 md:w-1/2 md:p-24">
-          <h1 className="text-4xl font-bold md:text-5xl">Bitz</h1>
-          <h2 className="mt-16 text-xl md:mt-4 md:text-2xl">{t('subtitle')}</h2>
-          <p className="mt-4 text-sm md:text-xl">{t('description')}</p>
-          <p className="mt-4 text-lg md:text-xl">{t('call')}</p>
+      <section className="mt-20 flex h-screen snap-start flex-col font-space_grotesk lg:flex-row">
+        <div className="flex w-screen flex-col justify-center p-4 lg:mr-12 lg:w-1/2 lg:p-24">
+          <h1 className="text-4xl font-bold lg:text-5xl">Bitz</h1>
+          <h2 className="mt-16 text-xl lg:mt-4 lg:text-2xl">{t('subtitle')}</h2>
+          <p className="mt-4 text-sm lg:text-xl">{t('description')}</p>
+          <p className="mt-4 text-lg lg:text-xl">{t('call')}</p>
           <div className="mt-8 flex space-x-4">
             <Link href="/auth/login">
               <Button variant="callToAction" size={'lg'}>
@@ -37,7 +37,7 @@ export default async function Home() {
             </Link>
           </div>
         </div>
-        <div className="mt-16 flex w-screen items-center justify-center rounded-full md:w-1/2">
+        <div className="flex h-1/2 w-screen items-center justify-center rounded-full lg:h-screen lg:w-screen lg:justify-center">
           <CubeScene />
         </div>
       </section>
@@ -46,19 +46,19 @@ export default async function Home() {
         id="next-section"
         className="relative flex h-screen snap-start flex-col items-center justify-center bg-blue-400 pb-[10vh] font-space_grotesk text-white"
       >
-        <h2 className="mb-8 text-4xl font-bold">Was machen wir anders?</h2>
+        <h2 className="mb-8 text-center text-4xl font-bold">{t('secondSectionTitle')}</h2>
         <div className="mt-4 flex flex-col items-center space-y-8 md:flex-row md:space-x-16 md:space-y-0">
           <div className="flex flex-col items-center">
             <Icons.shield className="h-48 w-96" />
-            <p className="mt-2 text-4xl">Sicherheit</p>
+            <p className="mt-2 text-4xl">{t('security')}</p>
           </div>
           <div className="flex flex-col items-center">
             <Icons.cable className="h-12 w-12" />
-            <p className="mt-2 text-4xl">Technik Fokus</p>
+            <p className="mt-2 text-4xl">{t('focus')}</p>
           </div>
           <div className="flex flex-col items-center">
             <Icons.paintbrush className="h-24 w-24" />
-            <p className="mt-2 text-4xl">Design</p>
+            <p className="mt-2 text-4xl">{t('design')}</p>
           </div>
         </div>
       </section>
@@ -66,59 +66,26 @@ export default async function Home() {
         id="circle-section"
         className="relative flex h-screen snap-start flex-col items-center justify-center bg-blue-950 pb-[10vh] text-white"
       >
-        <h2 className="mb-8 text-4xl font-bold">Wie Gehts?</h2>
+        <h2 className="mb-8 text-4xl font-bold">{t('thirdSectionTitle')}</h2>
         <div className="relative grid w-11/12 grid-cols-3 grid-rows-3 items-center justify-center">
           <div className="col-start-2 row-start-1 flex flex-col items-center justify-center text-center">
             <Icons.store className="h-12 w-12" />
-            <p className="mt-2 text-xl">Shop erstellen</p>
+            <p className="mt-2 text-xl">{t('createShop')}</p>
           </div>
           <div className="col-start-3 row-start-2 flex flex-col items-center justify-center text-center">
             <Icons.shoppingCart className="h-12 w-12" />
-            <p className="mt-2 text-xl">Produkte kaufen</p>
+            <p className="mt-2 text-xl">{t('buy')}</p>
           </div>
           <div className="col-start-2 row-start-3 flex flex-col items-center justify-center text-center">
             <Icons.handshake className="h-12 w-12" />
-            <p className="mt-2 text-xl">Technik-Produkte verkaufen</p>
+            <p className="mt-2 text-xl">{t('sell')}</p>
           </div>
           <div className="col-start-1 row-start-2 flex flex-col items-center justify-center text-center">
             <Icons.user className="h-12 w-12" />
-            <p className="mt-2 text-xl">Account erstellen</p>
+            <p className="mt-2 text-xl">{t('createAccount')}</p>
           </div>
         </div>
       </section>
     </main>
   )
-
-  // return (
-  //   <main className="static h-screen snap-y snap-mandatory overflow-y-scroll">
-  //     <section className="h-screen snap-start">
-  //       <div className="top h-96 pt-16 md:static md:h-screen md:pt-24">
-  //         <CubeScene />
-  //       </div>
-
-  //       <Link href={'/auth/login'}>
-  //         <div className="absolute bottom-24 right-7 h-24 w-72 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-8 font-space_grotesk text-4xl text-white hover:scale-105 hover:opacity-85 md:right-24 md:w-96 lg:h-24">
-  //           <div className="absolute right-6 mt-6 h-12 w-12 rounded-full bg-white md:right-9 md:mt-4 md:h-16 md:w-16">
-  //             <ChevronRightIcon className="relative size-12 text-black md:size-16" />
-  //           </div>
-  //           <div className="relative left-0 mt-7 text-3xl md:left-10">{t('button')}</div>
-  //         </div>
-  //       </Link>
-  //       <div className="static bottom-48 left-8 rounded-md p-5 font-space_grotesk text-black drop-shadow-2xl  md:left-24 lg:bottom-16 lg:w-7/12">
-  //         <p className="text-2xl md:text-5xl">
-  //           <em>Bitz</em>
-  //         </p>
-  //         <p className="mt-4 text-lg md:text-2xl">{t('subtitle')}</p>
-  //         <p className="mt-4 text-sm md:text-xl">{t('description')}</p>
-  //         <p className="mt-4 text-lg md:text-2xl">{t('call')}</p>
-  //       </div>
-  //     </section>
-  //     <section className="realtive h-screen snap-start flex-col items-center justify-center bg-red-600 pb-[10vh] text-[3.5vw] text-white">
-  //       Hello World!
-  //     </section>
-  //     <section className="realtive h-screen snap-start flex-col items-center justify-center bg-blue-600 pb-[10vh] text-[3.5vw] text-white">
-  //       Bye World!
-  //     </section>
-  //   </main>
-  // )
 }
