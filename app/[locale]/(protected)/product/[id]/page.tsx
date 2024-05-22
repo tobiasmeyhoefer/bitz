47e013ab76b68590c7259191e1ba6ad390e3ba50
Anna Chiara <anna.laves@web.de>
2024-05-22 16:47:19 +0200

    feat/product-details morechanges for pruduct deatils

--- app/[locale]/(protected)/product/[id]/page.tsx
+++ app/[locale]/(protected)/product/[id]/page.tsx
@@ -1,5 +1,15 @@
 import { ProductType } from '@/lib/types'
 import { ProductImageCarousel } from '../productImgCarousel'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from '@/components/ui/card'
+import { timestamp } from 'drizzle-orm/mysql-core'
 
 export default function Page({
   params,
@@ -10,14 +20,47 @@ export default function Page({
 }) {
   const productId = atob(params.id)
   const product: ProductType = JSON.parse(atob(searchParams.p))
+
+  const getDate = (timestamp: any) => {
+    const date = new Date(timestamp)
+
+    return (
+      <div>
+        {date.getDate()} {date.getMonth() + 1} {date.getFullYear()} {date.getHours()}:
+        {date.getMinutes()}
+      </div>
+    )
+  }
   return (
-    <div>
-      Some Product ID: {productId}
+    <div className="flex h-[70vh] w-full flex-col items-center justify-center lg:h-[60vh] lg:flex-row lg:justify-around">
       {/* <div>Title: {product.title}</div> */}
-      <br />
-      <br />
-      {product.title}
-      <ProductImageCarousel className="mx-auto" />
+      <ProductImageCarousel className="" size={600} />
+      {/* <div>
+        infos
+        <br />
+        Some Product ID: {productId}
+        <br />
+        {product.title}
+      </div> */}
+      <Card className="h-full w-[80vw] lg:w-[40vw]">
+        <CardHeader>
+          <CardTitle className="flex justify-between">
+            <div>{product.title}</div>
+            <div className="text-3xl">{product.price} $</div>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div>{product.description}</div>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          {getDate(product.createdAt)}
+          <br />
+          {product.createdAt?.toLocaleString()}
+          <br />
+          {product.createdAt?.toString()}
+        </CardFooter>
+        <div className="h-[44px]"></div>
+      </Card>
     </div>
   )
 }
--- app/[locale]/(protected)/product/productImgCarousel.tsx
+++ app/[locale]/(protected)/product/productImgCarousel.tsx
@@ -1,5 +1,5 @@
+'use client'
 import * as React from 'react'
-
 import { Card, CardContent } from '@/components/ui/card'
 import {
   Carousel,
@@ -7,61 +7,111 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
+  type CarouselApi,
 } from '@/components/ui/carousel'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
 
 export function ProductImageCarousel(props: any) {
+  //     <Carousel
+  //       className={cn('w-full max-w-lg', props.className)}
+  //       opts={{
+  //         loop: true,
+  //       }}
+  //       orientation="vertical"
+  //     >
+  //       <CarouselContent className="-ml-1">
+  //         {Array.from({ length: 5 }).map((_, index) => (
+  //           <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+  //             <div className="p-1">
+  //               <Card>
+  //                 <CardContent className="flex aspect-square items-center justify-center p-6">
+  //                   <span className="text-2xl font-semibold">{index + 1}</span>
+  //                 </CardContent>
+  //               </Card>
+  //             </div>
+  //           </CarouselItem>
+  //         ))}
+  //       </CarouselContent>
+  //       <CarouselPrevious />
+  //       <CarouselNext />
+  //     </Carousel>
+  //     <Carousel
+  //       opts={{
+  //         align: 'start',
+  //       }}
+  //       orientation="vertical"
+  //       className="w-full max-w-xs"
+  //     >
+  //       <CarouselContent className="-mt-1 h-[280px]">
+  //         {Array.from({ length: 5 }).map((_, index) => (
+  //           <CarouselItem key={index} className="">
+  //             <Card className="">
+  //               <CardContent className="p-0">
+  //                 <Image
+  //                   width={400}
+  //                   height={400}
+  //                   src="/test_img.jpg"
+  //                   alt="Product Image"
+  //                   className="rounded-xl "
+  //                 />
+  //               </CardContent>
+  //             </Card>
+  //           </CarouselItem>
+  //         ))}
+  //       </CarouselContent>
+  //       <CarouselPrevious />
+  //       <CarouselNext />
+  //     </Carousel>
+  const [api, setApi] = React.useState<CarouselApi>()
+  const [current, setCurrent] = React.useState(0)
+  const [count, setCount] = React.useState(0)
+
+  React.useEffect(() => {
+    if (!api) {
+      return
+    }
+
+    setCount(api.scrollSnapList().length)
+    setCurrent(api.selectedScrollSnap() + 1)
+
+    api.on('select', () => {
+      setCurrent(api.selectedScrollSnap() + 1)
+    })
+  }, [api])
+
   return (
-    //     <Carousel
-    //       className={cn('w-full max-w-lg', props.className)}
-    //       opts={{
-    //         loop: true,
-    //       }}
-    //       orientation="vertical"
-    //     >
-    //       <CarouselContent className="-ml-1">
-    //         {Array.from({ length: 5 }).map((_, index) => (
-    //           <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
-    //             <div className="p-1">
-    //               <Card>
-    //                 <CardContent className="flex aspect-square items-center justify-center p-6">
-    //                   <span className="text-2xl font-semibold">{index + 1}</span>
-    //                 </CardContent>
-    //               </Card>
-    //             </div>
-    //           </CarouselItem>
-    //         ))}
-    //       </CarouselContent>
-    //       <CarouselPrevious />
-    //       <CarouselNext />
-    //     </Carousel>
-    <Carousel
-      opts={{
-        align: 'start',
-      }}
-      orientation="vertical"
-      className="w-full max-w-xs"
-    >
-      <CarouselContent className="-mt-1 h-[280px]">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="">
-            <Card className="">
-              <CardContent className="p-0">
-                <Image
-                  width={400}
-                  height={400}
-                  src="/test_img.jpg"
-                  alt="Product Image"
-                  className="rounded-xl "
-                />
-              </CardContent>
-            </Card>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className="h-full">
+      <Carousel
+        setApi={setApi}
+        opts={{
+          loop: true,
+        }}
+      >
+        <CarouselContent className=" h-full w-[40vh]  md:h-[50vh] md:w-[50vh]  lg:w-[60vh]  xl:w-[70vh]">
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <Card className="h-fit w-fit">
+                <CardContent className="p-0">
+                  <Image
+                    width={500}
+                    height={500}
+                    src="/test_img.jpg"
+                    alt="Product Image"
+                    className="aspect-square h-full w-[40vh] rounded-xl object-cover md:h-[50vh] md:w-[50vh] lg:w-[60vh] xl:w-[70vh]"
+                    style={{ objectFit: 'cover' }}
+                  />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="py-2 text-sm text-muted-foreground">
+        Slide {current} of {count}
+      </div>
+    </div>
   )
 }
