1efbce3a520f6b72480195dfd27e4829b69bf73e
LucasK <l.knaeuper@gmail.com>
2024-05-23 15:53:50 +0200

    fix: aligned  responsive behavior from myShopContent with browseContent

--- components/myShop/myshopContent.tsx
+++ components/myShop/myshopContent.tsx
@@ -2,21 +2,18 @@
 import { useEffect, useState } from 'react'
 import { CardWithImage } from '@/components/ui/cardWithImage'
 import { getProductsOwned } from '@/lib/productaction'
-import {
-  ProductType,
-  MyShopProps
-} from '@/lib/types'
+import { ProductType, MyShopProps } from '@/lib/types'
 import RevealOnScroll from '../navigation/revealOnScroll'
 
 const MyShopContent = (props: MyShopProps) => {
   const [loading] = useState(false)
-  const [products, setProducts] = useState<ProductType[]>([]);
+  const [products, setProducts] = useState<ProductType[]>([])
 
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const result = await getProductsOwned(props.userId);
-        if(result) {
+        const result = await getProductsOwned(props.userId)
+        if (result) {
           const checkedResults: ProductType[] = result.map((item) => ({
             title: item.title,
             description: item.description ?? '',
@@ -29,22 +26,23 @@ const MyShopContent = (props: MyShopProps) => {
             imageUrl3: item.imageUrl3,
             imageUrl4: item.imageUrl4,
             imageUrl5: item.imageUrl5
-          }));
-          setProducts(checkedResults);
+          }))
+          setProducts(checkedResults)
         }
       } catch (error) {
-        console.error('Fehler beim Laden der Daten:', error);
+        console.error('Fehler beim Laden der Daten:', error)
       }
-    };
-    getProducts();
-  }, []);
+    }
+
+    getProducts()
+  }, [])
 
   let isProduct = (item: any) => item.price !== undefined
 
-  return  <>
-    <div className={`${loading && `h-full`}  `}>
+  return (
+    <div className={`${loading && `h-full`} flex flex-col items-center justify-center px-4 py-20 sm:px-10 md:px-[20px] lg:px-[30px] xl:px-[80px]`}>
       {!loading ? (
-        <div className="grid grid-cols-4 gap-8 mt-[20px]">
+        <div className="-mx-2 mt-[20px] flex flex-wrap justify-around overflow-y-hidden">
           {products.map((p, index) => (
             <RevealOnScroll key={`prx-${index}`}>
               <CardWithImage
@@ -52,8 +50,10 @@ const MyShopContent = (props: MyShopProps) => {
                 title={p.title}
                 desc={p.description}
                 imgUrl1={p.imageUrl1}
-                className=""
-
+                className="mx-[5px] my-[0.5rem]"
+                productID={p.id}
+                product={products[index]}
+                favIcon
               />
             </RevealOnScroll>
           ))}
@@ -64,8 +64,7 @@ const MyShopContent = (props: MyShopProps) => {
         </div>
       )}
     </div>
-    {/* <p>Current User ID: {props.userId}</p> */}
-  </>
+  )
 }
 
 export default MyShopContent
