fca05777eb87abd35d1c7c2b7f407736e5cf670f
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-05-25 19:32:18 +0200

    fix: testing wiht printing out

--- lib/verify-actions.ts
+++ lib/verify-actions.ts
@@ -36,6 +36,7 @@ export async function getVerificationNumber(): Promise<string> {
 //sends the sms
 export async function sendSmsToUser(number: string) {
   const verificationNumber = generateRandomSixDigitNumber()
+  console.log(process.env.TWILIO_ACCOUNT_SID!)
 
   const client = twilio(
     process.env.TWILIO_ACCOUNT_SID!,
