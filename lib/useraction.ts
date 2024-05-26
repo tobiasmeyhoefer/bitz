4b6e83de32022d6f77efb5935e5154c97a58df1c
Dennis Blömeke <de.bloemeke@gmail.com>
2024-05-26 17:43:38 +0200

    feat:added deleteYourAccount()

--- lib/useraction.ts
+++ lib/useraction.ts
@@ -2,7 +2,8 @@
 import { users } from '@/schema'
 import { db } from '../db'
 import { eq } from 'drizzle-orm'
-import { auth } from '@/auth'
+import { auth, signOut } from '@/auth'
+import { deleteImageOnAws, getProductsOwned } from './productaction'
 
 export async function saveUserLocation(values: { postcode: string }) {
   const session = await auth()
@@ -23,5 +24,34 @@ export async function getUser() {
   if (id) {
     const response = await db.select().from(users).where(eq(users.id, id))
     return response
+  }
+}
+
+// kann man vlt noch schöner machen
+export async function deleteYourAccount() {
+  const session = await auth()
+  const id = session?.user?.id
+  if (id) {
+    await signOut()
+    await db.delete(users).where(eq(users.id, id))
+    const products = await getProductsOwned(id)
+    // imageUrls wenn gesetzt aus AWS löschen
+    if (products) {
+      for (const product of products) {
+        const imageUrls = [
+          product.imageUrl1,
+          product.imageUrl2,
+          product.imageUrl3,
+          product.imageUrl4,
+          product.imageUrl5,
+        ]
+        for (const imageUrl of imageUrls) {
+          if (imageUrl) {
+            console.log(imageUrl)
+            await deleteImageOnAws(imageUrl)
+          }
+        }
+      }
+    }
   }
 }
