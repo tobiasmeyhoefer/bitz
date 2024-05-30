744459b67183cd14dbbb1e8db79236eff56df450
LucasK <l.knaeuper@gmail.com>
2024-05-31 01:03:00 +0200

    fix: removed useState

--- components/myShop/productDelAlert.tsx
+++ components/myShop/productDelAlert.tsx
@@ -12,21 +12,11 @@ import {
 import { Button } from "@/components/ui/button"
 import { FaTrash } from 'react-icons/fa';
 import { deleteProduct } from "@/lib/productaction";
-import { useEffect, useState } from "react";
 // import { useTranslations } from "next-intl";
 
 
 export function ProdDelAlert({ productId }: { productId: string })  {
-  //const t = useTranslations();
-    const [isOpen, setIsOpen] = useState(true)
-    useEffect(() => {
-    if (!isOpen) {
-      // Hier können Sie eine Funktion ausführen, wenn das Popup geschlossen wird
-      // Zum Beispiel können Sie die Seite aktualisieren
-      window.location.reload()
-    }
-    }, [isOpen])
-  
+  //const t = useTranslations();  
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
