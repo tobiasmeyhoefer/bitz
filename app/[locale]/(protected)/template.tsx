2b3416d5a723a3232881b2beddda17562b7147bc
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-06-06 22:01:48 +0200

    animation fix

--- app/[locale]/(protected)/template.tsx
+++ app/[locale]/(protected)/template.tsx
@@ -6,7 +6,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <LazyMotion features={domAnimation}>
       <m.div
-        initial={{ y: 20, opacity: 0 }}
+        initial={{ y: 0, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ ease: "easeInOut", duration: 0.75 }}
       >
