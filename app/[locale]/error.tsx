b838ac5254d8b70f6c0048315fadef955ff3b2a1
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-06-13 12:57:53 +0200

    simple error handling added

--- app/[locale]/error.tsx
+++ app/[locale]/error.tsx
@@ -1,7 +1,18 @@
 'use client' // Error components must be Client Components
- 
+
+import { Button } from '@/components/ui/button'
 import { useEffect } from 'react'
- 
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
 export default function Error({
   error,
   reset,
@@ -12,19 +23,41 @@ export default function Error({
   useEffect(() => {
     console.error(error)
   }, [error])
- 
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="mt-20 flex flex-col items-center gap-2">
+      <h1 className="text-xl font-bold">Irgendwas ist schiefgelaufen!</h1>
+      <p className="mb-5 text-center text-neutral-500">
+        Bitte versuche die Seite neuzuladen oder kehre zum Entdecken screen
+      </p>
+      <div className="flex gap-4">
+        <Button onClick={() => reset()}>Neu laden</Button>
+        <Button>Entdecken</Button>
+      </div>
+      {process.env.NODE_ENV === 'development' && (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="ghost">more info</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Error</DialogTitle>
+              <DialogDescription>Keine Sorge, dass sehen nur wir Entwickler</DialogDescription>
+            </DialogHeader>
+            <div>
+              <p className='text-sm'>Error Message: {error.message}</p>
+              <p className='text-[8px]'>Stack: {error.stack}</p>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="button" variant="secondary">
+                  Close
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      )}
     </div>
   )
 }
-
-;
