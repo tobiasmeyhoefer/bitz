24e876dcc8bebcb56c09a569c01910eff4e6e4f5
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-06-17 12:09:23 +0200

    fix with await pusherServer

--- app/api/message/route.ts
+++ app/api/message/route.ts
@@ -3,7 +3,7 @@ import { pusherServer } from '@/lib/pusher'
 export async function POST(req: Request) {
   const { content, convId, senderId } = await req.json()
 
-  pusherServer.trigger(convId, 'incoming-message', { content, senderId })
+  await pusherServer.trigger(convId, 'incoming-message', { content, senderId })
 
   return new Response(JSON.stringify({ success: true }))
 }
