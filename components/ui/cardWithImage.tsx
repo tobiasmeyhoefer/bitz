a51ff9612fc8a43a359563526ef23a40ab4d639b
LucasK <l.knaeuper@gmail.com>
2024-06-27 20:38:27 +0200

    fix delete-menu-position above price again

--- components/ui/cardWithImage.tsx
+++ components/ui/cardWithImage.tsx
@@ -20,9 +20,6 @@ const CardWithImage = React.forwardRef<HTMLDivElement, CardWithImageProps>(
       <>
         {props && product ? (
           <CardContainer>
-            <div className="absolute right-0 top-0 m-2">
-              {props.editable && props.favIcon && <ProdDelAlert productId={product.id} />}
-            </div>
             <Card className={cn(`w-[150px] md:w-[200px] lg:w-[300px]`, className)} ref={ref}>
               <Link href={`/product/${product.id}`}>
                 {product.imageUrl1 !== undefined ? (
@@ -62,7 +59,7 @@ const CardWithImage = React.forwardRef<HTMLDivElement, CardWithImageProps>(
                       </TooltipContent>
                     </Tooltip>
                   </TooltipProvider>
-
+                  {props.editable && props.favIcon && <ProdDelAlert productId={product.id} />}
                   {!props.editable && props.favIcon && <FavoriteLike productId={product.id} />}
                 </div>
                 <CardDescription className="text-sm">
