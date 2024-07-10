aac78e5427bdb9ae1597b298b6b818615fa257cb
Dennis Blömeke <de.bloemeke@gmail.com>
2024-07-10 12:35:30 +0200

    fix:cleanup

--- auth.ts
+++ auth.ts
@@ -5,8 +5,6 @@ import github from 'next-auth/providers/github'
 import passkey from 'next-auth/providers/passkey'
 import { drizzleAdapter } from './adapter'
 import { html, text } from './lib/auth-send-request'
-import { saveUserNameLogin } from './lib/user-actions'
-import { cookies } from 'next/headers'
 import axios from 'axios'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
@@ -38,16 +36,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
     passkey,
   ],
-  // events: {
-  //   createUser: async (message) => {
-  //     const name = cookies().get('name')
-  //     const email = cookies().get('email')
-  //     await saveUserNameLogin(name?.value!, email?.value!)
-  //     cookies().delete('name')
-  //     cookies().delete('email')
-  //     await axios.post('/api/mail/welcome', { to: email?.value! })
-  //   },
-  // },
   callbacks: {
     async session({ session, user }) {
       session.user.id = user.id
