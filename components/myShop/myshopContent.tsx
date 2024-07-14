9442963a5e32705d83513b5fbf141dc7d3b7ca06
Wolkenwolf9 <anton.kripp@gmail.com>
2024-07-14 11:22:29 +0200

    Fix/Y-Scroll on Cards

--- components/myShop/myshopContent.tsx
+++ components/myShop/myshopContent.tsx
@@ -17,7 +17,7 @@ const MyShopContent = async () => {
       className={`flex h-full flex-col items-center justify-center px-4 py-10 sm:px-10 md:px-[20px] lg:px-[30px] xl:px-[80px]`}
     >
       {
-        <div className="mx-2 mt-[20px] flex flex-wrap justify-around overflow-y-hidden">
+        <div className="mx-2 mt-[20px] flex flex-wrap justify-around overflow-hidden">
           {products?.map((p, index) => (
             // <AnimatedCard  delay={0.3} >
             <CardWithImageMyShop
