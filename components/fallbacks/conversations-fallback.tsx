a9661c52cd198be94f5dbcee2967a7364b02710e
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-07-17 11:19:35 +0200

    transaction/conversation skeleton shortened

--- components/fallbacks/conversations-fallback.tsx
+++ components/fallbacks/conversations-fallback.tsx
@@ -79,44 +79,6 @@ export const ConversationLoadingSkeleton = () => (
           <div>
             <div className="flex flex-col space-y-1.5 p-6">
               <h3 className="leading-none tracking-tight">
-                <Skeleton className="w-[384px] max-w-full" />
-              </h3>
-            </div>
-            <div className="flex flex-col items-start p-6 pt-0">
-              <p>
-                <Skeleton className="w-[152px] max-w-full" />
-              </p>
-            </div>
-          </div>
-        </div>
-      </a>
-      <a>
-        <div className="relative flex h-[140px] border">
-          <div>
-            <SVGSkeleton className="h-[140px] min-h-[140px] w-[140px] min-w-[140px]" />
-          </div>
-          <div>
-            <div className="flex flex-col space-y-1.5 p-6">
-              <h3 className="leading-none tracking-tight">
-                <Skeleton className="w-[352px] max-w-full" />
-              </h3>
-            </div>
-            <div className="flex flex-col items-start p-6 pt-0">
-              <p>
-                <Skeleton className="w-[152px] max-w-full" />
-              </p>
-            </div>
-          </div>
-        </div>
-      </a>
-      <a>
-        <div className="relative flex h-[140px] border">
-          <div>
-            <SVGSkeleton className="h-[140px] min-h-[140px] w-[140px] min-w-[140px]" />
-          </div>
-          <div>
-            <div className="flex flex-col space-y-1.5 p-6">
-              <h3 className="leading-none tracking-tight">
                 <Skeleton className="w-[456px] max-w-full" />
               </h3>
             </div>
